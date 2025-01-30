095b12dc01f3476898f73aa61713b3258d294c48
AmeePansuriya <ameepansuriya.it22@scet.ac.in>
2025-01-30 04:12:18 +0000

    commit

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='main'>
+      <h1>you can not try to maked the my contact bcz you are not konwn me.</h1>
+      <h2>here you go to contact me : <a href='0123456789' className='context'>0123456789</a></h2>
+    </div>
+  )
+}
+
+export default Contact
